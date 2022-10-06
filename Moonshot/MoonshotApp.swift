20895665a3c1a9b0408671cddf2efb14438c4380
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-06 10:42:56 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Zaid Neurothrone on 2022-10-06.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
